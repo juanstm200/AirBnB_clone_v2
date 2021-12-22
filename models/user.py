efba73e241d152f62a85755c2a6833e240072bd4
juanstm200 <juans.tovar70@gmail.com>
2021-12-21 23:59:31 -0800

    User_models - Python

--- models/user.py
+++ models/user.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""Defines the User class."""
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import String
+"""This module defines a class User"""
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """Represents a user for a MySQL database."""
-    __tablename__ = "users"
+    """This class defines a user by various attributes"""
+    __tablename__ = 'users'
     email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
+    password = Column(String(128), nullable=False)  # guipwd error
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship('Place', backref='user')
+    reviews = relationship('Review', backref='user')
