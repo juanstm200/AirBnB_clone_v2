3180caae93cf98c5f6dd33e8ae2cae2f28e81fdc
juanstm200 <juans.tovar70@gmail.com>
2021-12-21 23:56:21 -0800

    state_models -Python

--- models/state.py
+++ models/state.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-"""Defines the State class."""
-import models
-from os import getenv
-from models.base_model import Base
-from models.base_model import BaseModel
-from models.city import City
-from sqlalchemy import Column
-from sqlalchemy import String
+""" State Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """Represents a state for a MySQL database"""
+    """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City",  backref="state", cascade="delete")
+    cities = relationship('City', backref='states')
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
-        @property
-        def cities(self):
-            """Get a list of all related City objects."""
-            city_list = []
-            for city in list(models.storage.all(City).values()):
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+    @property
+    def cities(self):
+        """ getter attribute cities that returns the list of City
+        instances with state_id equals to the current State.id"""
+        from models import storage
+        from models.city import City
+        list = []
+        cities = storage.all(City)
+        for value in cities.values():
+            if value.state_id == self.id:
+                list.append(value)
+        return list
