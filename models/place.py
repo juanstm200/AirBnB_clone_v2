8fe2d84be52e6ca8f8a653c2600cf3e3aba1e4bb
jihuder <julioarenas85@gmail.com>
2021-12-21 22:20:47 -0500

    Style

--- models/place.py
+++ models/place.py
@@ -35,7 +35,7 @@ class Place(BaseModel, Base):
                            cascade="delete")
 
     amenities = relationship("Amenity", secondary="place_amenity",
-                                    viewonly=False)
+                             viewonly=False)
     amenity_ids = []
 
     if getenv("HBNB_TYPE_STORAGE") != "db":
