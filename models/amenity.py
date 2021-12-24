c9aa73e34c8b8841d5c9e4e27a34716c5d1c4d1c
juanstm200 <juans.tovar70@gmail.com>
2021-12-23 18:32:09 -0800

    _amenitys_ - Python

--- models/amenity.py
+++ models/amenity.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-"""Defines the Amenity class."""
-from models.base_model import Base
+""" State Module for HBNB project """
+import models
 from models.base_model import BaseModel
+from models.base_model import Base
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """Represents an Amenity for a MySQL database"""
+    """ Amenity: Class"""
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary="place_amenity",
                                    viewonly=False)
+
+    def __init__(self, *args, **kwargs):
+        """
+            Init for inherited
+        """
+        super().__init__(*args, **kwargs)
