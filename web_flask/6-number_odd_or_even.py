b471d1b4d7a0fdaa8cfe9ceac5ad14945bdab51a
juanstm200 <juans.tovar70@gmail.com>
2022-02-03 04:46:34 +0000

    [fix] - number

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -53,5 +53,6 @@ def number_odd_or_even_route(n=None):
             oe = "even"
         return render_template("6-number_odd_or_even.html", n=n, oe=oe)
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000) 
